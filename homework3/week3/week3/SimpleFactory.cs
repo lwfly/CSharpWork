bdc68e28599e9014c487211d0a0250728b2d0e85
w l <lw1650862004@outlook.com>
2020-03-10 19:03:19 +0800

    first

---
+++ homework3/week3/week3/SimpleFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week3
+{
+    class SimpleFactory
+    {
+        private static Random s = new Random();
+
+        public static Graphical NewShape(string type)
+        {
+            switch (type.ToLower())
+            {
+                case "rectangle":
+                    return new Rectangle(10 * s.NextDouble(), 10 * s.NextDouble());
+                case "square":
+                    return new Square(10 * s.NextDouble());
+                case "triangle":
+                    return new Triangle(10 * s.NextDouble(), 10 * s.NextDouble(), 10 * s.NextDouble());
+                default:
+                    throw new ArgumentException(message: "error");
+            }
+        }
+
+        public static string GetResult()
+        {
+            string[] shapes = { "rectangle", "square", "triangle" };
+            return shapes[s.Next(0, 3)];
+        }
+
+    }
+
+}
